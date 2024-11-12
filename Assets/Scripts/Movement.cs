60c5c531c505480f51712a1276df1d167ac10600
emilyharris1 <emily.harris-2@colorado.edu>
2024-11-12 10:12:17 -0700

    first commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+    public InputActionReference triggerLeft;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Proposal.md
@@ -0,0 +1,2 @@
+Emily Harris VR Final Project Proposal: Underwater Odessey
+https://o365coloradoedu-my.sharepoint.com/:w:/g/personal/emha7676_colorado_edu/ETfGuVrpbpFFkzb6gQg_DNYBFtfHAYVhqrqdKFbnmRSKUA?e=rJfKre
